068d656b56f7798f5cfa40ba5feb6822e5710448
Ahmedur Rahman Shovon <shovon.sylhet@gmail.com>
2026-01-28 14:45:38 -0600

    [refactor] remove unnecessary parameter

--- examples/dataflow/run.c
+++ examples/dataflow/run.c
@@ -18,9 +18,9 @@ static void *handler(const char *s)
     return NULL;
 }
 
-int main(int argc, char **argv[])
+int main()
 {
-    assert(drava_init(&argc, &argv) == DRAVA_SUCCESS);
+    assert(drava_init() == DRAVA_SUCCESS);
     assert(drava_register_routine(handler) == DRAVA_SUCCESS);
     assert(drava_listen() == DRAVA_SUCCESS);
     assert(drava_deinit() == DRAVA_SUCCESS);
